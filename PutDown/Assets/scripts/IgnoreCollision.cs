e478ca1f2f97bee43d859f591c39a73bdd5010a8
Igor Potard <igor.potard@epita.fr>
2023-01-14 18:01:48 +0400

    debut

---
+++ PutDown/Assets/scripts/IgnoreCollision.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dc684a9558a3f19638019f9240748836691f47f0aab8705937bac6a4440ee235
+size 785
---
+++ PutDown/Assets/scripts/PlayerController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:046d3f235d11d1b16e316657736ecb219da52cb7f58dd9699aaaa6a882fd24fa
+size 3110
